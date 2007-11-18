e47f0e5feff7023036a797a69e55cd8c2e29bea7
arobirosa <arobirosa@257a94f0-f839-0410-8eff-614ca9f59e5b>
2007-11-18 18:42:16 +0000

    Pagina de Busqeuda de Viajes de Conductores

--- trunk/latincarpooling/php/header.php
+++ trunk/latincarpooling/php/header.php
@@ -1,70 +1,22 @@
 <?php
 function carpooling_header($title){
-echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>'.$title.'</title>
-<style type="text/css">
-<!--
-.style0 {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-}
-.style1 {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	font-weight: bold;
-}
-.style2 {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	background-color:#F4F4F4;
-}
-.style3 {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	background-color:#FF0000;
-}
-.style4 {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-}
-.style5 {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	font-weight: bold;
-	color: #FF0000
-}
-.styleTituloColumna {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	background-color:#F4F4F4;
-	font-weight: bold;
-	text-align: center;
-}
-
-.sinLink {
-  color: black;
-  text-decoration: none;
-}
-.styleOk {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	background-color:#00FF00;
-	text-align: center;
-}
-.styleFalla {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	background-color:#FF0000;
-	text-align: center;
-}
--->
-</style>
-</head>
-
-<body>
+    echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+	    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+    <head>
+        <title>'.$title.'</title>
+        <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+        <meta name="description" content="Sitio de Carpooling Latinoamericano" />
+        <meta name="keywords" content="your,keywords,goes,here" />
+        <meta name="author" content="Grupo 3 de SABD / Original design by Andreas Viklund - http://andreasviklund.com" />
+        <link rel="stylesheet" href="andreas09.css" type="text/css" media="screen" />
+    </head>
+    <body>
+        <div id="container">
+            <div id="sitename">
+                <h1>Viajo y Comparto</h1>
+                <h2>Compartiendo la experiencia de recorrer Latinoamerica</h2>
+            </div>
 ';
 }
 ?>
--- trunk/latincarpooling/php/menu.php
+++ trunk/latincarpooling/php/menu.php
@@ -1,69 +1,47 @@
 <?Php
-/* PK - 16/06/2007 - Imprime las paginas de un tipo de categoria (C=Configuracion, M=Monitoreo, I=Informe */
-function item($tipo) {
-	$result=pg_query("select distinct p.idpagina, p.descripcion, p.url
-	from usuarios U, UsuariosListasPermisos UP, ListasPermisos LP, PaginasListasPermisos PP, paginas P
-	where P.idpagina = PP.idpagina
-	and U.idusuario = UP.idusuario
-	and LP.idlistapermiso = PP.idlistapermiso
-	and LP.idlistapermiso = UP.idlistapermiso
-	and U.idusuario = ".$_SESSION["idusuario"]."
-	and P.tipo = '".$tipo."'
-	order by p.descripcion");	
-	
-	while ($row = pg_fetch_row($result)){			
-		echo '		<tr>
-						<td bgcolor="#999999" height="1">
-						</td>
-					</tr>
-					<tr> 
-						<td bgcolor="#F4F4F4" align="right" onmouseover=\'this.style.background="#CCCCCC"\' onmouseout=\'this.style.background="#F4F4F4"\'>
-							<table cellpadding="0" cellspacing="0" width="150" >
-								<tr>
-									<td align="left">
-										<span class="style0">
-											<a href="'.$row[2].'" class="sinLink">'.$row[1].'</a>
-										</span>
-									</td>
-								</tr>
-							</table>
-						</td>									
-					</tr>
-			';
-	}
-}
 
-/* PK - 16/06/2007 - Imprime el título de una categoría */
-function categoria($titulo){
-echo '			<tr>
-					<td bgcolor="#999999" height="1">
-					</td>
-				</tr>
-				<tr>					
-					<td bgcolor="#DCDCDC">
-						<span class="style1">'.$titulo.'</span>
-					</td>
-				</tr>';
-}
-
-/* PK - 16/06/2007 - Imprime el menu izquierdo */
 function menu(){
-require('/var/www/include/global.php');
+echo '<div id="mainmenu">
+<ul>
+<li><a class="current" href="index.html">Inicio</a></li>
+<li><a href="green.html">Viajes</a></li>
+<li><a href="orange.html">Pedidos</a></li>
+<li><a href="purple.html">Mensajes</a></li>
+<li><a href="red.html">Opciones</a></li>
+<li><a href="red.html">Salir</a></li>
+<li><a href="red.html">Novedades</a></li>
+</ul>
+</div>
+ 
+<div id="wrap">
 
-$cn=pg_connect("host=localhost dbname=".$postgres_database." user=".$postgres_user." password=".$postgres_pass);
+<div id="leftside">
+<a class="nav active" href="index.php">Inicio</a><span class="hide"> | </span>
+<a class="nav" href="2col.html">Viajes</a><span class="hide"> | </span>
+<a class="nav sub" href="buscarviaje.php">Buscar</a><span class="hide"> | </span>
+<a class="nav sub" href="#">Mis Viajes</a><span class="hide"> | </span>
+<a class="nav" href="no-img.html">Pedidos</a><span class="hide"> | </span>
+<a class="nav sub" href="#">Buscar</a><span class="hide"> | </span>
+<a class="nav sub" href="#">Mis Pedidos</a><span class="hide"> | </span>
+<a class="nav" href="#">Mis Mensajes</a><span class="hide"> | </span>
+<a class="nav sub" href="#">Recibidos</a><span class="hide"> | </span>
+<a class="nav sub" href="#">Enviados</a><span class="hide"> | </span>
+<a class="nav sub" href="#">Nuevo</a><span class="hide"> | </span>
+<a class="nav" href="#">Opciones</a><span class="hide"> | </span>
+<a class="nav" href="#">Salir</a><span class="hide"> | </span>
+<a class="nav" href="#">Novedades</a><span class="hide"> | </span>
 
-echo '      <br/>
-			<!--PK - 02/06/2007 - Inicio Menu -->
-			<table cellpadding="0" cellspacing="0" width="153" >	';	
-categoria('Monitoreo');				
-item('M');
-categoria('Informes');	
-item('I');
-categoria('Configuración');
-item('C');	
-echo '	</table>
-			<!--PK - 02/06/2007 - Fin Menu -->
- ';
-pg_close($cn);
-}
+</div>
+
+<div id="rightside">
+<h1>Latest news</h1>
+<p><strong>Dec 11: </strong>
+andreas09 is built with valid XHTML 1.1 and CSS2. It is simple and easy to work with, and it comes in several different flavors. This is version 1.0 (Dec 11th 2005).</p>
+<p><strong>Dec 10: </strong>
+Place for news or important messages, maybe?</p>
+
+</div>
+';
+};
+
 ?>
